58cb56e15b2a846905ad3d5583b7dd3f8fd1bdb0
youndong lee <bloodntear@gmail.com>
2023-03-20 16:46:54 +0900

    delete sidebar

--- file.py
+++ file.py
@@ -10,13 +10,11 @@ with open('secrets.toml', 'r') as f:
 
 openai.api_type = "azure"
 openai.api_key = os.environ["OPENAI_API_KEY"]
-openai.api_base = "https://bloodntear.openai.azure.com//"
+openai.api_base = "https://bloodntear.openai.azure.com/"
 openai.api_version = "2022-12-01"
 
-st.set_page_config(page_title="Custom MTC ChatGPT", page_icon="💬")
-
-st.markdown("# Custom MTC ChatGPT")
-st.sidebar.header("Custom MTC ChatGPT")
+st.set_page_config(page_title="쬐그만한 ChatGPT", page_icon="💬")
+st.markdown("# 쬐그만한 ChatGPT")
 
 #generating 2 empty lists to store past and generated value in the conversation
 
@@ -25,26 +23,23 @@ if 'generated' not in st.session_state:
 
 if 'past' not in st.session_state:
     st.session_state['past'] = []
-
-user_input = st.text_input("You: ","Hello, how are you?", key="input")
+user_input = st.text_input("You: ","안녕하세요", key="input")
 
 if user_input:
     output = openai.Completion.create(
-          engine="test1",
+          engine="bloodntear",
           prompt=f"{st.session_state.past}\n{user_input}",
-          temperature=0,
-          max_tokens=4001,
+          temperature=0.6,
+          max_tokens=3000,
           top_p=1,
-          frequency_penalty=0,
-          presence_penalty=0,
+          frequency_penalty=1,
+          presence_penalty=1,
           best_of=1,
           stop=None)
-
     st.session_state.past.append(user_input)
     st.session_state.generated.append(output["choices"][0]["text"].strip())
 
 if st.session_state['generated']:
-
     for i in range(len(st.session_state['generated'])-1, -1, -1):
         message(st.session_state["generated"][i], avatar_style = 'bottts', key=str(i))
         message(st.session_state['past'][i], avatar_style = 'big-ears',is_user=True, key=str(i) + '_user')
